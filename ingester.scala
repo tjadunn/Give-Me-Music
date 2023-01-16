c5af200a1dff2a2009472d487a3eb91e0922f55b
Toby Dunn <toby@timehri.world>
2023-01-16 17:58:09 +0000

    Extend inheritance to allow for more generic sources

--- ingester.scala
+++ ingester.scala
@@ -1,7 +1,7 @@
 // Ingest messages from a Kafka topic into Structured Streaming
 
 import org.apache.spark.sql.functions.{from_json,col}
-import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.{Row, DataFrame, ForeachWriter}
 import org.apache.spark.sql.types.{MapType, StringType}
 
 // Get streaming data
@@ -10,12 +10,44 @@ import org.apache.spark.sql.types.{MapType, StringType}
 // if so -> call API which returns resp with music info
 //
 
-val df: DataFrame = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "localhost:9092").option("subscribe", "musical_tweets").load()
-val df2: DataFrame = df.selectExpr("CAST(value AS STRING)").withColumn("value",from_json(col("value"),MapType(StringType, StringType)))
-df2.writeStream.foreach(...).format("console").option("truncate", false).start().awaitTermination()
+trait StreamIngester {
 
-def get_url_component(row: DataSet[Row], batchId: Long) => {
-  //regex url from dataframe
-  // send req to api
-  // post back the song(s)
+  class for_each_writer extends ForeachWriter[Row] {
+    def process(value: Row): Unit
+
+    def open(paritionId: Long, epochId: Long): Boolean
+
+    def close(errorOrNull: Throwable): Unit
+  }
+}
+
+class KafkaStramIngester extends StreamIngester {
+
+  val dataframe_kafka_read: DataFrame =
+    spark.readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "localhost:9092")
+      .option("subscribe", "musical_tweets")
+      .load()
+
+  val mapped_dataframe: DataFrame = dataframe_kafka_read
+    .selectExpr("CAST(value AS STRING)")
+    .withColumn("value",from_json(col("value"),MapType(StringType, StringType)))
+
+  class for_each_writer extends ForeachWriter[Row] {
+    override def process(value: Row): Unit
+      = println(s"We are going to process this value soon {$value}")
+
+    override def open(partitionId: Long, epochId: Long): Boolean = true
+
+    override def close(errorOrNull: Throwable): Unit = {}
+  }
+
+  mapped_dataframe.writeStream.foreach(new for_each_writer).option("truncate", false).start().awaitTermination()
+
+  def get_url_component(row: DataSet[Row], batchId: Long) => {
+    //regex url from dataframe
+    // send req to api
+    // post back the song(s)
+  }
 }
