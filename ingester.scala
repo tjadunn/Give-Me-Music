faa39b349839e8f1efafc2300956e491c36242e8
Toby Dunn <toby@timehri.world>
2023-01-16 22:03:51 +0000

    This commit does a couroot_ple of things * Make our interface extendable - we probably are going to need different writers * Further abstract the StreamIngestion - I reckon we're going to want to   add read_stream() etc etc to this also

--- ingester.scala
+++ ingester.scala
@@ -4,21 +4,29 @@ import org.apache.spark.sql.functions.{from_json,col}
 import org.apache.spark.sql.{Row, DataFrame, ForeachWriter}
 import org.apache.spark.sql.types.{MapType, StringType}
 
-// Get streaming data
-// map each row to a Map Type
-// find out wether each row contains a url
-// if so -> call API which returns resp with music info
-//
+// Define an abstract class for any stream
+abstract class StreamIngester {
+  def run_stream(): Unit
+}
 
-trait StreamIngester {
-
-  class for_each_writer extends ForeachWriter[Row] {
+// Define a side effect abstract class to perform some write action for each dataframe Row
+abstract class foreach_row_writer extends ForeachWriter[Row] {
+    // Define any writing here e.g to S3 or just print
     def process(value: Row): Unit
 
+    // Any IO we have to do
     def open(paritionId: Long, epochId: Long): Boolean
 
     def close(errorOrNull: Throwable): Unit
-  }
+}
+
+class row_printer extends foreach_row_writer{
+    override def process(value: Row): Unit
+      = println(s"We are going to process this value soon {$value}")
+
+    override def open(partitionId: Long, epochId: Long): Boolean = true
+
+    override def close(errorOrNull: Throwable): Unit = {}
 }
 
 class KafkaStramIngester extends StreamIngester {
@@ -34,18 +42,16 @@ class KafkaStramIngester extends StreamIngester {
     .selectExpr("CAST(value AS STRING)")
     .withColumn("value",from_json(col("value"),MapType(StringType, StringType)))
 
-  class for_each_writer extends ForeachWriter[Row] {
-    override def process(value: Row): Unit
-      = println(s"We are going to process this value soon {$value}")
-
-    override def open(partitionId: Long, epochId: Long): Boolean = true
-
-    override def close(errorOrNull: Throwable): Unit = {}
-  }
-
-  mapped_dataframe.writeStream.foreach(new for_each_writer).option("truncate", false).start().awaitTermination()
+  def run_stream(): Unit =
+    mapped_dataframe
+      .writeStream
+      .foreach(new row_printer)
+      .option("truncate", false)
+      .start()
+      .awaitTermination()
 
   def get_url_component(row: DataSet[Row], batchId: Long) => {
+    // TODO
     //regex url from dataframe
     // send req to api
     // post back the song(s)
